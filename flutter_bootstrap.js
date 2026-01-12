723b5aa77e7a64133e6c31603b88f241b4bc884b
Tony <tony@insoftonline.de>
2026-01-13 02:09:30 +0300

    Separate public and private projects and add new projects
    Change so many UI elements

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "47919802"
+    serviceWorkerVersion: "2556911549"
   }
 });
