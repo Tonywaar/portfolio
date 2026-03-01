4b163db1b63dafa3899fb2c8f06e5a83d45eb33c
Tony <tony@insoftonline.de>
2026-03-02 02:09:17 +0300

    update information

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1451223536"
+    serviceWorkerVersion: "1531315381"
   }
 });
